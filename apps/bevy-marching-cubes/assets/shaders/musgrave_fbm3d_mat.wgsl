20a84c5752d9abd9248778b02f04800b0734717d
dugi <dugi@channel.io>
2025-01-30 15:05:21 +0900

    feat(bevy-marching-cubes): add base for musgrave fBm3d

---
+++ apps/bevy-marching-cubes/assets/shaders/musgrave_fbm3d_mat.wgsl
@@ -0,0 +1,8 @@
+#import bevy_pbr::forward_io::VertexOutput
+
+@fragment
+fn fragment(
+    in: VertexOutput,
+) -> @location(0) vec4<f32> {
+    return vec4<f32>(1.0, 1.0, 1.0, 1.0);
+}
--- apps/bevy-marching-cubes/src/main.rs
+++ apps/bevy-marching-cubes/src/main.rs
@@ -36,13 +36,16 @@ fn main() {
     app.add_plugins(MaterialPlugin::<SimplexNoiseMaterial>::default());
     app.add_systems(Update, simplex_noise_update_uniforms);
 
+    app.add_plugins(MaterialPlugin::<MusgraveMaterial>::default());
+    app.add_systems(Update, musgrave_update_uniforms);
+
     app.run();
 }
 
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<SimplexNoiseMaterial>>,
+    mut materials: ResMut<Assets<MusgraveMaterial>>,
 ) {
     commands.spawn((
         Camera3d::default(),
@@ -51,7 +54,7 @@ fn setup(
 
     commands.spawn((
         Mesh3d(meshes.add(Rectangle::new(1.0, 1.0))),
-        MeshMaterial3d(materials.add(SimplexNoiseMaterial::default())),
+        MeshMaterial3d(materials.add(MusgraveMaterial::default())),
         Transform::from_xyz(0.0, 0.0, 0.0)
             .with_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
     ));
@@ -75,5 +78,67 @@ fn simplex_noise_update_uniforms(
 ) {
     for mat in materials.iter_mut() {
         mat.1.time = Vec4::new(time.elapsed_secs(), time.delta_secs(), 0.0, 0.0);
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct MusgraveConfig {
+    offset: Vec3,
+    scale: f32,
+    detail: u32,
+    dimension: f32,
+    lacunarity: f32,
+}
+
+impl Default for MusgraveConfig {
+    fn default() -> Self {
+        Self {
+            offset: Vec3::ZERO,
+            scale: 1.0,
+            detail: 5,
+            dimension: 2.0,
+            lacunarity: 2.0,
+        }
+    }
+}
+
+impl From<MusgraveConfig> for Vec4 {
+    fn from(config: MusgraveConfig) -> Self {
+        Vec4::new(
+            config.scale,
+            config.detail as f32,
+            config.dimension,
+            config.lacunarity,
+        )
+    }
+}
+
+#[derive(Debug, Clone, AsBindGroup, Asset, TypePath, Default)]
+struct MusgraveMaterial {
+    #[uniform(0)]
+    offset: Vec3,
+
+    #[uniform(1)]
+    config_uniform: Vec4,
+
+    config: MusgraveConfig,
+}
+
+impl MusgraveMaterial {
+    fn update_uniforms(&mut self) {
+        self.offset = self.config.offset;
+        self.config_uniform = self.config.into();
+    }
+}
+
+impl Material for MusgraveMaterial {
+    fn fragment_shader() -> ShaderRef {
+        "shaders/musgrave_fbm3d_mat.wgsl".into()
+    }
+}
+
+fn musgrave_update_uniforms(mut materials: ResMut<Assets<MusgraveMaterial>>) {
+    for mat in materials.iter_mut() {
+        mat.1.update_uniforms();
     }
 }
